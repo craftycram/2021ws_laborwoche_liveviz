e2ae2dce845b4bed721fb9f874c52e75489a9fc3
craftycram <github@craftycram.net>
2020-11-09 16:54:21 +0100

    added p5 color preview

--- vizualization/sketch.js
+++ vizualization/sketch.js
@@ -1,9 +1,29 @@
 let ws = new WebSocket('ws://127.0.0.1:1880/ws/recieve');
 
+let bgColor = {
+    r: 0,
+    g: 0,
+    b: 0,
+};
+
 ws.onopen = (event) => {
     console.log(event);
 }
 
 ws.onmessage = (msg) => {
-    console.log(msg);
+    const data = JSON.parse(msg.data);
+    bgColor.r = data.r;
+    bgColor.g = data.g;
+    bgColor.b = data.b;
+    console.log(bgColor);
+    // document.writeln(JSON.stringify(data));
 }
+
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(color(bgColor.r, bgColor.g, bgColor.b));
+  }
